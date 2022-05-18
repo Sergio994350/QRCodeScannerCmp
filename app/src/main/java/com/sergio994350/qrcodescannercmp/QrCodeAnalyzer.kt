fa7acb2b1812672dc0c8cc04848fdd7efe8a27d2
Sergey Tarasov <tarasov-mnm@yandex.ru>
2022-05-18 09:21:40 +0300

    add qr code analyzer

--- app/src/main/java/com/sergio994350/qrcodescannercmp/QrCodeAnalyzer.kt
+++ app/src/main/java/com/sergio994350/qrcodescannercmp/QrCodeAnalyzer.kt
@@ -3,11 +3,13 @@ package com.sergio994350.qrcodescannercmp
 import android.graphics.ImageFormat
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.zxing.*
+import com.google.zxing.common.HybridBinarizer
 import java.nio.ByteBuffer
 
 class QrCodeAnalyzer(
     private val onQrCodeScanned: (String) -> Unit
-): ImageAnalysis.Analyzer {
+) : ImageAnalysis.Analyzer {
 
     private val supportedImageFormats = listOf(
         ImageFormat.YUV_420_888,
@@ -16,12 +18,40 @@ class QrCodeAnalyzer(
     )
 
     override fun analyze(image: ImageProxy) {
-        if(image.format in supportedImageFormats) {
-            val bytes = image.planes.first().buffer
+        if (image.format in supportedImageFormats) {
+            val bytes = image.planes.first().buffer.toByteArray()
+
+            val source = PlanarYUVLuminanceSource(
+                bytes,
+                image.width,
+                image.height,
+                0, 0,
+                image.width,
+                image.height,
+                false
+            )
+            val binaryBmp = BinaryBitmap(HybridBinarizer(source))
+            try {
+                val result = MultiFormatReader().apply {
+                    setHints(
+                        mapOf(
+                            DecodeHintType.POSSIBLE_FORMATS to arrayListOf(
+                                BarcodeFormat.QR_CODE
+                            )
+                        )
+                    )
+                }.decode(binaryBmp)
+                onQrCodeScanned(result.text)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                image.close()
+            }
         }
     }
 
     private fun ByteBuffer.toByteArray(): ByteArray {
         rewind()
+        return ByteArray(remaining()).also { get(it) }
     }
 }
